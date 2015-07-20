6c6fbe07a81921b7019fe9e2630a4b361bfb7861
ckhatri <chirag.k.khatri@gmail.com>
2015-07-20 12:20:02 -0500

    Finished header

--- wp-content/themes/html5blank/index.php
+++ wp-content/themes/html5blank/index.php
@@ -1,3 +1,30 @@
 <?php get_header(); ?>
-    <h1>Hello, world!</h1>
+<header>
+	<div class = "container headerInfo">
+		<div class = "headerInfo vertical-align">
+			<div class = "row text-center">
+				<div class = "col-md-12">
+					<h1>PRODUCT NAME</h1>
+				</div>
+				<div class = "col-md-12">
+					<h2>Lorem Ipsum, Ipsum Lorem</h2>
+				</div>
+				<div class = "col-md-12">
+					<hr/>
+					<hr id ="linetwo"/>
+				</div>
+				<!-- Spacer! -->
+				<div class = "col-md-3"></div>
+				<div class = "col-md-3">
+					<button class ="btn btn-lg">Item goes here</button>
+				</div>
+				<div class = "col-md-3">
+					<button class ="btn btn-lg">Visit us on Kickstarter!</button>
+				</div>
+				<!-- Spacer! -->
+				<div class = "col-md-3"></div>
+			</div>
+		</div>
+	</div>
+</header>
 <?php get_footer(); ?>
