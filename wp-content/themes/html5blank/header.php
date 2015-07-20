0fc95e930e14a30783576b0ac95b730cb04bb80e
ckhatri <chirag.k.khatri@gmail.com>
2015-07-20 11:07:14 -0500

    Fixed reference to css stylesheet

--- wp-content/themes/html5blank/header.php
+++ wp-content/themes/html5blank/header.php
@@ -2,13 +2,12 @@
 <head>
 <title>crowdfunding</title>
 <link rel="shortcut icon" href="/favicon.ico" />
-<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
 <script src="//use.typekit.net/yjl6qkx.js"></script>
 <script>try{Typekit.load();}catch(e){}</script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 </head>
 <body>
